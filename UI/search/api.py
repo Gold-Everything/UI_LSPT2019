88fb41cf6032bdc6fb7d1d7fb50af984c08b3054
Adam Gibbons <gibboa@rpi.edu>
2019-11-29 13:14:45 -0500

    added naive implementation of getRawResults to api.py as well as some skeleton for calling api funcs in views.py

--- UI/search/api.py
+++ UI/search/api.py
@@ -14,8 +14,49 @@ def search(request):
     weights = request.GET.get('weights', '')
     return HttpResponse(f"You searched for {query} with weights {weights}")
 
-# def getRawResults(query, weights):
+def getRawResults(query, weights):
+    '''
+    Function to be trigger by query request, will call callDocstore and callRanking
+    Inputs: Form data from user: string query, list of weights
+    Returns:
+       JSON of merged data of the form: 
+       { rank :  
+             { 
+               "id": <id>, 
+               "score": <score>,
+               "url": <url>, 
+               "title": <title>,
+               "body": <body>
+             },
+             ...
+       }
+    '''
+    rank_results = callRanking(query, weights)
+    rank_results_dict = json.loads(rank_results) #convert json of urls->scores to dictionary
+    #as of python 3.7 dict should maintain order of insertions so assuming that
+    #docIds_list will be in order of url ranking high to lowi
+    docIds_list = rank_results_dict.keys()
+    doc_results = callDocstore(docIds_list)
+    doc_results_dict = json.loads(doc_results)
 
+    #merge both jsons from callDocstore and callRanking and return result
+    total_results = len(docIds_list)
+    merged_results = {}
+    for rank in range (1, total_results+1):
+        merged_results[rank] = {} #adding key and empty dict for current rank 
+        #append ID
+        merged_results[rank]["id"] = docIds_list[rank-1]
+        #append score
+        merged_results[rank]["score"] = rank_results_dict[docIds_list[rank-1]]
+        #append URL (assumes list of results from DDS are also in ascending sorted
+        #order by rank
+        merged_results[rank]["url"] = doc_results_dict["documents"][rank-1]["url"]
+        #append title (same assumption as for url)
+        merged_results[rank]["title"] = doc_results_dict["documents"][rank-1]["title"]
+        #append doc text (same assumption as for url)
+        merged_results[rank]["body"] = doc_results_dict["documents"][rank-1]["body"]
+    
+    return json.dumps(merged_results)
 
 # def makeResults(rawDocuments):
 
@@ -36,6 +77,20 @@ def callRanking(query, weights):
         raise Exception("No response from ranking")
 
 def callDocstore(docIds):
+    '''
+    DDS wants us to specify the fields we want,
+    http://docdatastore/documents?id=someID,anotherID,andtheotherIDs&fields=url,title,body
+    response will look like
+    {
+      "documents": [
+          {
+              "url": "some url"
+              "title": "RPI Computing",
+              "body": "html source here...",
+          }
+      ]
+    }
+    '''
     # https://errose28.github.io/lspt-doc-data-store/#get
     # Call to data store is a GET
     response = requests.get(RANKING_API_URL, params={'id': docIds})
--- UI/search/views.py
+++ UI/search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import api
+
 
 def index(request):
     template = loader.get_template('index.html')
@@ -21,4 +23,26 @@ def index(request):
     '''
 
 def search_results(request):
-    return HttpResponse('Here are your results')
+    '''
+    I think this is the entry point for running our API calls and generating search results?
+    We do the following:
+    1. pull up the template (html file for results)
+    2. set the context using the results of calling getRawResults and makeResults
+    '''
+    template = loader.get_template('results.html')
+    
+    #not sure what the request argument is or how to get the form data
+    raw_results = api.getRawResults(query, weights)
+    
+    finale_result = api.makeResults(raw_results)
+
+    #add a function to transform the list of jsons returned from makeResults into the actual context Dict?
+    context = transformResults(final_results)
+
+    #context = {
+        # 'showings_list': showings_list,
+    #}
+    
+    return HttpResponse(template.render(context, request))
+    
+    #return HttpResponse('Here are your results')
